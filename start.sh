de826bc425e57b1357a2153cb94733d74df579cb
Roman Valls Guimera <brainstorm@nopcode.org>
2020-06-08 00:22:40 +1000

    Mouse/keyboard do not seem to be responding on the Jetson Nano, enabling udev countermeasures outlined in https://forums.balena.io/t/x11-hotplug-usb/7250/31

--- start.sh
+++ start.sh
@@ -6,17 +6,15 @@ export DISPLAY=:0
 export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 
 # USB mouse/keyboard not responding on container: https://forums.balena.io/t/x11-hotplug-usb/7250/30
-#if which udevadm > /dev/null; then
-#  set +e # Disable exit on error
-#  udevadm control --reload-rules
-#  service udev restart
-#  udevadm trigger
-#  set -e # Re-enable exit on error
-#fi
+if which udevadm > /dev/null; then
+  set +e # Disable exit on error
+  udevadm control --reload-rules
+  service udev restart
+  udevadm trigger
+  set -e # Re-enable exit on error
+fi
 
-echo "Starting X in 2 seconds"
-sleep 2
-#startx /usr/src/app/openpnp/openpnp.sh 
+echo "Starting X"
 startx
 
 while :

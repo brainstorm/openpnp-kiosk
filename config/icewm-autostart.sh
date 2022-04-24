8a37856d66d71b9fd20e1c2ac9b2f2fc502f4885
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2022-04-24 17:26:30 +1000

    Replace the current icewm process

--- config/icewm-autostart.sh
+++ config/icewm-autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 LD_LIBRARY_PATH=/usr/lib/aarch64-linux-gnu/jni /opt/openpnp/OpenPnP &
-/usr/bin/icewmbg -i /usr/src/app/cchs_wallpaper.png &
+/usr/bin/icewmbg -p -i /usr/src/app/cchs_wallpaper.png &
